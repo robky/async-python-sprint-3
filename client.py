7912f832c3253eaae8efdc2ba1f8903c62fc3ffc
Pavel Gaidukov <mailgpn@gmail.com>
2023-03-08 23:18:19 +0500

    server + client

--- client.py
+++ client.py
@@ -1,10 +1,105 @@
-# import asyncio
-# import aiohttp
+import asyncio
+import logging
+import sys
+from asyncio import StreamReader, StreamWriter
+from dataclasses import dataclass
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s.%(msecs)03d [%(name)s]: %(message)s",
+    datefmt="%d-%m-%Y %H:%M:%S",
+)
+logger = logging.getLogger("client")
+# stop_event = asyncio.Event()
 
 
+@dataclass
 class Client:
-    def __init__(self, server_host="127.0.0.1", server_port=8000):
-        pass
+    server_host: str = "127.0.0.1"
+    server_port: int = 8000
+    _reader: StreamReader = None
+    _writer: StreamWriter = None
+    _client_is_work: bool = False
 
-    def send(self, message=""):
-        pass
+    async def _send(self, message: str = "") -> None:
+        if not self._client_is_work:
+            await self._close()
+            return
+        logger.debug(f"Send: {message}")
+        # message += "\n"
+        try:
+            self._writer.write(message.encode())
+            await self._writer.drain()
+        except Exception as e:
+            logger.error(f"Dont sent message: {e}")
+            await self._close()
+
+    async def _read(self) -> None:
+        while self._client_is_work:
+            try:
+                data = await self._reader.read(100)
+            except ConnectionResetError:
+                break
+            if not data:
+                break
+            print(data.decode().strip())
+        await self._close()
+
+    # async def _read_starter(self):
+    #     while self._client_is_work:
+    #         await asyncio.wait([self._read_stdin2()], timeout=5.0)
+    #
+    # async def _read_stdin2(self):
+    #     rand_delay = 1.5
+    #     print(f"Generated rand {rand_delay}...")
+    #     await asyncio.sleep(rand_delay)
+    #     print(f"Coro finished {rand_delay}...")
+    #     return rand_delay
+
+    async def _read_stdin(self) -> None:
+        loop = asyncio.get_event_loop()
+        while self._client_is_work:
+            # try:
+            #     future = loop.run_in_executor(None, sys.stdin.readline)
+            #     line = await asyncio.wait_for(future, 5, loop=loop)
+            # except asyncio.futures.TimeoutError:
+            #     logger.debug("Timeout")
+            #     continue
+
+            line = await loop.run_in_executor(None, sys.stdin.readline)
+            await self._send(line)
+
+    async def _open(self) -> None:
+        self._reader, self._writer = await asyncio.open_connection(
+            self.server_host, self.server_port
+        )
+        self._client_is_work = True
+
+    async def _close(self) -> None:
+        if self._client_is_work:
+            self._writer.close()
+            self._client_is_work = False
+
+    async def start(self) -> None:
+        await self._open()
+        tasks = [
+            self._read(),
+            self._read_stdin(),
+            # self._read_starter()
+        ]
+        try:
+            await asyncio.gather(*tasks)
+        except Exception as e:
+            logger.error(f"Error with the server: {e}")
+        finally:
+            logger.info("Close the connection")
+
+
+if __name__ == "__main__":
+    logger.info("Start program client")
+    try:
+        asyncio.run(Client().start())
+    except Exception as e:
+        logger.error(f"Lost connection with the server: {e}")
+    finally:
+        logger.info("Stop program client")
--- server.py
+++ server.py
@@ -1,9 +1,251 @@
-# import asyncio
+import logging
+from asyncio import StreamReader, StreamWriter, run, start_server
+from collections import namedtuple
+from dataclasses import dataclass, field
+
+SERVER_NAME = "server"
+CIRCLE_LIST_SIZE = 20
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s.%(msecs)03d [%(name)s]: %(message)s",
+    datefmt="%d-%m-%Y %H:%M:%S",
+)
+logger = logging.getLogger("server")
+
+User = namedtuple("User", ["name", "address", "writer"])
 
 
+@dataclass
+class CircleList:
+    size: int = CIRCLE_LIST_SIZE
+    _head: int = 0
+    _tail: int = -1
+    _circle = []
+
+    def add(self, value):
+        if len(self._circle) < self.size:
+            self._circle.append(value)
+            self._tail += 1
+            return
+        self._tail = (self._tail + 1) % self.size
+        self._circle[self._tail] = value
+        if self._head == self._tail:
+            self._head = (self._head + 1) % self.size
+
+    def get(self):
+        if self._tail < 0:
+            return []
+        index = self._head
+        result = []
+        while index != self._tail:
+            result.append(self._circle[index])
+            index = (index + 1) % self.size
+        result.append(self._circle[self._tail])
+        return result
+
+
+@dataclass
 class Server:
-    def __init__(self, host="127.0.0.1", port=8000):
-        pass
+    host: str = "127.0.0.1"
+    port: int = 8000
+    _passport: dict[str, dict[int, StreamWriter]] = field(default_factory=dict)
+    _last_messages: CircleList = CircleList()
 
-    def listen(self):
-        pass
+    async def client_connected(
+        self, reader: StreamReader, writer: StreamWriter
+    ):
+        _, address = writer.get_extra_info("peername")
+        user = User(None, address, writer)
+        logger.info(f"The new Client {user.address} is connected")
+        encoding_err_msg = "Only utf-8 encoding is supported."
+        not_send_msg = "The message has not been sent."
+
+        while True:
+            message = "Welcome. Enter your name:"
+            await self._write(user, message)
+
+            try:
+                input_date = await reader.readline()
+                user = user._replace(name=input_date.decode().strip())
+                logger.info(
+                    f"The Client {user.address} called himself [{user.name}]"
+                )
+                break
+            except ConnectionResetError:
+                await self._user_out(user)
+                return
+            except UnicodeDecodeError:
+                await self._write(user, encoding_err_msg)
+
+        message = (
+            f"Hello [{user.name}]. Enter /q to exit or enter /h if you "
+            f"need help."
+        )
+        await self._write(user, message)
+
+        if user.name in self._passport:
+            self._passport.get(user.name).update({user.address: user.writer})
+        else:
+            self._passport[user.name] = {user.address: user.writer}
+            message = f"The new user [{user.name}] has just connected"
+            await self._write_all(message, user)
+
+        await self._write_last_messages(user)
+
+        while True:
+            try:
+                input_date = await reader.readline()
+            except ConnectionResetError:
+                break
+            if not input_date:
+                break
+            try:
+                input_msg = input_date.decode().strip()
+            except UnicodeDecodeError:
+                await self._write(user, encoding_err_msg + not_send_msg)
+                continue
+            logger.debug(
+                f"User [{user.name}]:{user.address} received: {input_msg}"
+            )
+            if "/" in input_msg:
+                await self._command(user, input_msg)
+            else:
+                await self._write_all(input_msg, user, from_server=False)
+
+        await self._user_out(user)
+
+    async def _write(self, user: User, message: str) -> bool:
+        message = message.strip() + "\r\n"
+        try:
+            user.writer.write(message.encode())
+            await user.writer.drain()
+        except ConnectionResetError:
+            await self._user_out(user)
+            return False
+        return True
+
+    async def _write_last_messages(self, user: User) -> None:
+        messages = self._last_messages.get()
+        for message in messages:
+            await self._write(user, message)
+
+    async def _write_from_server(self, user: User, message: str) -> None:
+        message = f"[{SERVER_NAME}] {message}"
+        await self._write(user, message)
+
+    async def _write_all(
+        self, message: str, except_for: User = None, from_server: bool = True
+    ):
+        if from_server:
+            name = SERVER_NAME
+        else:
+            name = except_for.name
+        message = f"[{name}] {message.strip()}"
+        if from_server is False:
+            self._last_messages.add(message)
+        for user_name, client_writers in self._passport.items():
+            for address, writer in client_writers.items():
+                if except_for and except_for.address == address:
+                    continue
+                await self._write(User(user_name, address, writer), message)
+
+    async def _write_private(
+        self, user_from: User, user_to: str, message: str
+    ) -> bool:
+        if user_to not in self._passport:
+            message_error = f"User {user_to} not found"
+            await self._write(user_from, message_error)
+            return False
+        user_to_writers = self._passport[user_to]
+        user_to_writers.update(self._passport.get(user_from.name, {}))
+        number_of_sent = 0
+        message = f"[{user_from.name}] -> [{user_to}] {message}"
+        for address, writer in user_to_writers.items():
+            if address == user_from.address:
+                continue
+            result = await self._write(User(user_to, address, writer), message)
+            if result:
+                number_of_sent += 1
+        return True if number_of_sent else False
+
+    async def _command(self, user: User, message: str) -> None:
+        index = message.find("/") + 1
+        command = message[index: index + 1].lower()
+        message = message[index + 1:]
+        error_message = "Input error"
+        if not command:
+            await self._write_from_server(user, error_message)
+            return
+        match command:
+            case "q":
+                await self._user_out(user)
+            case "h":
+                mes = [
+                    "Possible actions are described below:",
+                    "/h - The help (this messages).",
+                    "/q - To exit.",
+                    "/p <user> <message> - Private message for the <user>",
+                ]
+                for send_message in mes:
+                    await self._write_from_server(user, send_message)
+            case "p":
+                parts_messages = message.split()
+                if len(parts_messages) < 2:
+                    await self._write_from_server(user, error_message)
+                    return
+                user_to = parts_messages[0]
+                if user_to == user.name:
+                    msg_err_yourself = "You can't send a message to yourself!"
+                    await self._write(user, msg_err_yourself)
+                    return
+                send_message = " ".join(parts_messages[1:])
+                result = await self._write_private(user, user_to, send_message)
+                if result:
+                    logger.debug(
+                        f"User [{user.name}]:{user.address} received to "
+                        f"[{user_to}]: {send_message}"
+                    )
+                else:
+                    error_message = (
+                        f"The message for the [{user_to}] was " f"not sent"
+                    )
+                    await self._write_from_server(user, error_message)
+                    logger.debug(
+                        f"User [{user.name}]:{user.address} do not received "
+                        f"to [{user_to}]: {send_message}"
+                    )
+            case _:
+                error_message = "Unknown command or input error"
+                await self._write_from_server(user, error_message)
+
+    async def _user_out(self, user: User) -> None:
+        user.writer.close()
+        user_clients = self._passport.get(user.name)
+        if user_clients is None:
+            return
+        if user.address in user_clients:
+            del self._passport[user.name][user.address]
+            logger.info(
+                f"Stop serving [{user.name}] for address: {user.address}"
+            )
+        if len(self._passport.get(user.name)) == 0:
+            del self._passport[user.name]
+            message = f"The user [{user.name}] has left"
+            logger.info(f"The user: [{user.name}] quit")
+            await self._write_all(message, user)
+
+    async def listen(self):
+        srv = await start_server(self.client_connected, self.host, self.port)
+        logger.info(
+            f"The server is running and waiting for connection on "
+            f"{self.host}:{self.port}"
+        )
+
+        async with srv:
+            await srv.serve_forever()
+
+
+if __name__ == "__main__":
+    logger.info("Start program server")
+    run(Server().listen())
